ad744c0cb2f98ec640949a0007936f2faba8fae2
violet978 <villette@live.com>
2022-10-22 09:13:03 -0700

    Corrected heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async';
 
 
 const Home = () => {
   return (
+    <div>
+    <Helmet>
+    <title>Home</title>
+    <meta name='description' content='Home' />
+    </Helmet>
+
     <h2>Home goes here</h2>
+    </div>
   )
 }
 
